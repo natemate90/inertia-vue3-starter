7f86f2932bd62647eefb086988bdf5aa8ff2db30
Nathan Alder <nathan.alder@gmail.com>
2022-11-21 16:11:10 +0100

    first commit

---
+++ resources/views/layouts/default.vue
@@ -0,0 +1,7 @@
+
+<template>
+  <div class="min-h-screen container mx-auto border border-t-0 border-b-0 border-blue-500">
+    The default layout
+    <slot />
+  </div>
+</template>
---
+++ resources/views/pages/index.vue
@@ -0,0 +1,12 @@
+
+<template layout>
+	<div>
+        <h1 class="text-3xl font-bold">{{page}}</h1>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    page: string
+}>()
+</script>
